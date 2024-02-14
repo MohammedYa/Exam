ca89df7e0b050b430ac3d4162eaa881366260e63
MuhamedElsafy <muhamedd.kh@gmail.com>
2024-02-14 22:28:18 +0200

    image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componants/home/home.component';
+import { NavbarComponent } from './componants/navbar/navbar.component';
+import { SidebarComponent } from './componants/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
