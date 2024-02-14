d441599004d6b31364291b1b9e7f401c8089191e
MuhamedElsafy <muhamedd.kh@gmail.com>
2024-02-15 01:30:00 +0200

    form one done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componants/home/home.component';
@@ -13,7 +13,9 @@ import { SidebarComponent } from './componants/sidebar/sidebar.component';
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
