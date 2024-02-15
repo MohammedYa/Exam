0bf636d4637efb561c8ce0fe3c7dd9b97a4ca919
MohammedYa <mohamedel3sawy4110@gmail.com>
2024-02-15 23:57:07 +0200

    add

---
+++ src/app/componants/committees/committees.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-committees',
+  templateUrl: './committees.component.html',
+  styleUrl: './committees.component.scss'
+})
+export class CommitteesComponent {
+
+}
